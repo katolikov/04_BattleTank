0bd8902036678c866bcf0dd423ca4d82f802fa56
Mark0f <artemmarkov1998@gmail.com>
2017-11-22 21:40:08 +0300

    Starting to setup movement of the tank

---
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/World.h"
+#include "TankTrack.generated.h"
+
+// Component of our Tracks with properties
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// Set throttle between -1 and 1
+	UFUNCTION(BlueprintCallable, Category = Input)
+		void SetThrottle(float Throttle);	
+};
