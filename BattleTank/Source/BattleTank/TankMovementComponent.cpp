be94b663570265efb6f2ed916e92596de3771b66
Mark0f <artemmarkov1998@gmail.com>
2017-12-02 18:59:16 +0300

    Tank movement is completed

--- BattleTank/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -6,15 +6,23 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
+	// TODO prevent from double speeding if we use W and A&D at the same time
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 	// TODO prevent from double speeding if we use W and A&D at the same time
 }
 
